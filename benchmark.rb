9325ab035ef5fbf6c935de74a5374957e23f7e7a
VGabby <hoangbach2311@gmail.com>
2015-06-28 13:35:28 +0700

    Add benchmark example

---
+++ benchmark.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+require 'benchmark'
+str = Benchmark.measure do
+	10_000_000.times do
+			"test"			 
+	end
+end.total
+sym = Benchmark.measure do
+	10_000_000.times do
+			:test	
+	end
+end.total 
+puts "String: "   + str.to_s 
+puts "Symbol: " + sym.to_s
+
